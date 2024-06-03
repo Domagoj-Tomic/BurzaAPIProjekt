15de32bf9397eeb07e150f93da32fd63dd1f0e49
BG0451 <thisisamailforemails@gmail.com>
2024-06-03 20:41:40 +0200

    Update RequestHandler.class.php

--- system/util/RequestHandler.class.php
+++ system/util/RequestHandler.class.php
@@ -1,12 +1,12 @@
 <?php
 class RequestHandler {
-        //ovo sve je kopirano nepromjenjeno iz vježbe 6
         function __construct($className)
         {
                 $className = $className . "Page";
+                // Include-anje AbstractPage-a
+                require_once(ROOT . '/system/control/AbstractPage.class.php');
                 require_once(ROOT . "/system/control/" . $className . ".class.php");
                 new $className();
-                echo "Ako vidiš ovo, RequestHandler.class.php je ok.<br>";
         }
 
         public static function handle(){
