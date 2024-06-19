fed06cb11c895b4a26ad373cd3ab0203e21073bd
Domagoj Tomić <domagoj.tomic135@gmail.com>
2024-06-19 23:20:38 +0200

    mali bug

--- tests/CreateRecordPageTest.php
+++ tests/CreateRecordPageTest.php
@@ -2,10 +2,10 @@
 
 use PHPUnit\Framework\TestCase;
 
-require_once(dirname(__DIR__) . "/system/AppCore.class.php"); 
-require_once(dirname(__DIR__) . "/system/model/MySQLiDatabase.class.php"); 
-require_once(dirname(__DIR__) . "/system/control/AbstractPage.class.php"); 
-require_once(dirname(__DIR__) . "/system/control/CreateRecordPage.class.php"); 
+require_once(dirname(__DIR__) . "/system/AppCore.class.php");
+require_once(dirname(__DIR__) . "/system/model/MySQLiDatabase.class.php");
+require_once(dirname(__DIR__) . "/system/control/AbstractPage.class.php");
+require_once(dirname(__DIR__) . "/system/control/CreateRecordPage.class.php");
 
 class CreateRecordPageTest extends TestCase
 {
@@ -28,31 +28,31 @@ class CreateRecordPageTest extends TestCase
         $database = $this->createConfiguredMock(
             MySQLiDatabase::class,
             [
-                'sendQuery' => 'table',
+                'sendQuery' => (object) ['num_rows' => 0],
             ],
-            );
-            $timeSeries = array(
-                2024-01-25 => [
-                    "1. open"=>"184.96",
-                    "2. high"=>"196.90",
-                    "3. low"=>"184.83",
-                    "4. close"=>"190.43",
-                    "5. volume"=>"29596239"
-                ],
-                2024-01-26 => [
-                    "1. open"=>"184.96",
-                    "2. high"=>"196.90",
-                    "3. low"=>"184.83",
-                    "4. close"=>"190.43",
-                    "5. volume"=>"29596239"
-                ]
-            );
+        );
+        $timeSeries = array(
+            2024 - 01 - 25 => [
+                "1. open" => "184.96",
+                "2. high" => "196.90",
+                "3. low" => "184.83",
+                "4. close" => "190.43",
+                "5. volume" => "29596239"
+            ],
+            2024 - 01 - 26 => [
+                "1. open" => "184.96",
+                "2. high" => "196.90",
+                "3. low" => "184.83",
+                "4. close" => "190.43",
+                "5. volume" => "29596239"
+            ]
+        );
 
         // Act
         $createRecord->db = $database;
-        $response = $createRecord->createTableAndInsertData("IBM",$timeSeries);
+        $response = $createRecord->createTableAndInsertData("IBM", $timeSeries);
 
         // Assert
-        $this->assertEquals('Table for IBM created.',$response);
+        $this->assertEquals('Table for IBM created.', $response);
     }
 }
